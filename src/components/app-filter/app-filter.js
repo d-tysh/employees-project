020a761eea60e5f45fc195a05e373b51cc8e6d86
Dima <you@example.com>
2022-11-20 15:28:20 +0200

    first commit

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,23 +1,31 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all', label:'Все сотрудники', colored: false},
+        {name: 'rise', label:'На повышение', colored: false},
+        {name: 'moreThan1000', label:'З/П больше 1000$', colored: true}
+    ];
+
+    const buttons = buttonsData.map(({name, label, colored}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        const style = colored ? {color: 'red'} : null;
+
+        return (
+            <button type='button'
+                    className={`btn ${clazz}`}
+                    key={name}
+                    onClick={() => props.onFilterSelect(name)}
+                    style={style} >
+                    {label}
+            </button>
+        )
+    });
+
     return (
         <div className="btn-group">
-            <button 
-                className="btn btn-light"
-                type='button'>
-                    Все сотрудники
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type='button'>
-                    На повышение
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type='button'>
-                    З/П больше 1000$
-            </button>
+            {buttons}
         </div>
     );
 };
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,24 +1,48 @@
 import './employees-list-item.css';
+// import data from '../app/app';
 
-const EmployeesListItem = () => {
+const EmployeesListItem = (props) => {
+    const {name, salary, onDelete, onToggleProp, increase, rise} = props;
+
+    let classNames = 'list-group-item d-flex justify-content-between';
+    if (increase) {
+        classNames += ' increase';
+    }
+
+    if (rise) {
+        classNames += ' like';
+    }
+
     return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue='1000$'/>
+        <li className={classNames}>
+            <span className='list-group-item-label' 
+                  onClick={onToggleProp} 
+                  data-toggle='rise'
+                //   style={{fontSize: '40px', 
+                //           color: 'red', 
+                //           transition: 'all', 
+                //           WebkitTransition: 'all', 
+                //           msTransition: 'all'
+                //         }}
+                  >{name} </span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className="d-flex justify-content-center align-items-center">
                 <button type='button'
-                    className="btn-cookie btn-sm">
+                    className="btn-cookie btn-sm"
+                    onClick={onToggleProp}
+                    data-toggle='increase'>
                     <i className="fas fa-cookie"></i>
                 </button>
 
                 <button type='button'
-                    className="btn-trash btn-sm">
+                        className="btn-trash btn-sm"
+                        onClick={onDelete} >
                     <i className="fas fa-trash"></i>
                 </button>
                 <i className="fas fa-star"></i>
             </div>
         </li>
     );
-};
+}
 
 export default EmployeesListItem;
